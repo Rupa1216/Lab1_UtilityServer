d31b20a39d9ef933682b56cdcdba38d153e0d3d2
Srirupa Datta <srirupadatta@pursuit.org>
2018-12-30 04:21:03 -0500

    updated server's math routes' responses

--- app.js
+++ app.js
@@ -10,22 +10,72 @@ const port = 8080;
 
 app.get('/', (req, res) => {
     console.log('You called ', req.query);
-    res.send(`Hello`);
+    res.send(`Hello!`);
 });
 
+// Math Routes:
 app.get('/math/add', (req, res) => {
-    console.log('You called ', req.query);
-    res.send(`You called add`);
+
+    const a = parseInt(req.query.a);
+    const b = parseInt(req.query.b);
+    console.log(a, b);
+
+    const result = math.add(a, b);
+    const returnObj = {};
+    
+    returnObj.input = req.query;
+    returnObj.sumString = `${a} + ${b}`;
+    returnObj.sum = result;
+
+    res.json(returnObj);
 });
+
+/* Expected response:
+{
+  input: {
+    a: 2,
+    b: 3
+  },
+  sumString: '2 + 3',
+  sum: 5
+}
+*/
 
 app.get('/math/multiply', (req, res) => {
-    console.log('You called ', req.query);
-    res.send(`You called multiply`);
+    
+    const a = parseInt(req.query.a);
+    const b = parseInt(req.query.b);
+    const c = parseInt(req.query.c);
+    console.log(a, b, c);
+
+    const result = math.multiply(a, b, c);
+    const returnObj = {};
+    
+    returnObj.input = req.query;
+    returnObj.productString = `${a} * ${b} * ${c}`;
+    returnObj.product = result;
+
+    res.json(returnObj);
 });
 
-// app.get('/image', (req, res) => {
+/*
+Expected Response:
+{
+  input: {
+    cat: 50,
+    b: 3,
+    zoo: 10
+  },
+  productString: '50 * 3 * 10',
+  product: 1500
+}
+*/
 
-//     request(`https://avatars0.githubusercontent.com/u/953172?s=460&v=4`, { encoding: null }, function (error, response, body) {
+// Search Gif Route:
+// app.get('/gif/', (req, res) => {
+
+//     request(`https://avatars0.githubusercontent.com/u/953172?s=460&v=4`, function (error, response, body) {
+//         console.log('error:', error);
 //         res.contentType('jpeg');
 //         res.send(body);
 //     });
--- services/math.js
+++ services/math.js
@@ -3,6 +3,6 @@ const fs = require('fs');
 const math = {};
 
 math.add = (a,b) => a + b;
-math.multiply = (a,b) => a * b;
+math.multiply = (a,b, c) => a * b * c;
 
 module.exports = math;
