adf2e6570360938f381fec3b987ee56d780055dc
Srirupa Datta <srirupadatta@pursuit.org>
2018-12-29 23:50:49 -0500

    defined server routes

--- app.js
+++ app.js
@@ -8,10 +8,29 @@ const app = express();
 
 const port = 8080;
 
+app.get('/', (req, res) => {
+    console.log('You called ', req.query);
+    res.send(`Hello`);
+});
+
 app.get('/math/add', (req, res) => {
     console.log('You called ', req.query);
     res.send(`You called add`);
 });
+
+app.get('/math/multiply', (req, res) => {
+    console.log('You called ', req.query);
+    res.send(`You called multiply`);
+});
+
+// app.get('/image', (req, res) => {
+
+//     request(`https://avatars0.githubusercontent.com/u/953172?s=460&v=4`, { encoding: null }, function (error, response, body) {
+//         res.contentType('jpeg');
+//         res.send(body);
+//     });
+
+// });
 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
